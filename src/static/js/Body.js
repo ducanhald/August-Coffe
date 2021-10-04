4bf4b13bf27e61a2e3cdb5bbd4352a695cd735aa
ducanhald <ducanhald@gmail.com>
2021-10-04 21:40:25 +0700

    First commit

---
+++ src/static/js/Body.js
@@ -0,0 +1,5 @@
+function Bpdy(){
+    return(
+        
+    )
+}
---
+++ src/static/js/Header.js
@@ -0,0 +1,46 @@
+import React from "react";
+
+function Header() {
+  const date = new Date();
+  date.return(
+    <React.Fragment>
+      <div className="header">
+        <div className="header__logo">
+          <img
+            src="../assets/imgs/logo.png"
+            className="header__img-logo"
+            alt=""
+          />
+        </div>
+        <div className="header__menu">
+          <ul className="nav">
+            <li>
+              <a href="https://www.facebook.com/anhs0708/">Home</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/anhs0708/">About</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/anhs0708/">Product</a>
+            </li>
+            <li>
+              <a href="https://www.facebook.com/anhs0708/">Service</a>
+            </li>
+          </ul>
+        </div>
+        <div className="header__search">
+          <input
+            type="text"
+            className="input-search"
+            placeholder="Search ..."
+          />
+          <img src="./assets/imgs/search.png" className="img-search" alt="" />
+          <div className="div-menu">
+            <i className="ti-menu-alt s-icon-menu"></i>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+export default Header;
