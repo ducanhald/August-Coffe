3f87ea12da163c047e95ba8548327531d74a46bd
ducanhald <ducanhald@gmail.com>
2021-10-21 23:47:10 +0700

    commit add home

---
+++ src/static/js/Home.js
@@ -0,0 +1,23 @@
+function Home() {
+  return (
+    <div class="home ">
+      <div class="home__about">
+        <p class="brand-name">August Coffe</p>
+        <p class="brand-title">
+          Tháng 8, tháng của sự đặc biệt bởi khoảng khắc giao mùa được hoà lẫn
+          giữa sự bồn chồn đón lấy mùa thu và sự nuối tiếc khi mùa hạ đã chẳng
+          còn ở đó. Còn gì tuyệt vời hơn khi ghé lại với chúng tôi thưởng thức
+          một ly cafe giữa đen và trắng, rồi cảm nhận bước chân của thời gian
+          đang đi qua kí ức. Để rồi không còn những muộn phiền, chỉ còn trong
+          tim sự bình yên đến lạ thường.
+        </p>
+        <div class="div-btn-sign">
+          <button id="btn-signup">Đăng Kí</button>
+          <button id="btn-signin">Đăng Nhập</button>
+        </div>
+      </div>
+      <div class="home__banner"></div>
+    </div>
+  );
+}
+export default Home;
